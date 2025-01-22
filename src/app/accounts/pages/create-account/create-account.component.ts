b0ed72dfe9eb990cbfd5da97507aebd6708efe22
ManuelGonzalez <manuel_agh@hotmail.com>
2025-01-22 16:40:22 -0600

    create account layout

--- src/app/accounts/pages/create-account/create-account.component.ts
+++ src/app/accounts/pages/create-account/create-account.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Actions } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { AppState } from '@store/reducers';
+import { CardModule } from 'primeng/card';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-create-account',
-  imports: [],
+  imports: [
+    CardModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    InputNumberModule,
+    FloatLabelModule,
+    ButtonModule,
+  ],
   templateUrl: './create-account.component.html',
-  styleUrl: './create-account.component.scss'
+  styleUrl: './create-account.component.scss',
 })
 export class CreateAccountComponent {
-
+  store$ = inject(Store<AppState>);
+  actions$ = inject(Actions);
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    initialBalance: new FormControl(null, [Validators.required]),
+  });
 }
