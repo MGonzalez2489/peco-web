121b37f52528c0f7631a0ba3f7f92ec0fffc3f9c
ManuelGonzalez <manuel_agh@hotmail.com>
2025-02-27 20:50:21 -0600

    removed general sidenav and migrated error msg component to use signals

--- src/app/_shared/components/information/error-message/error-message.component.ts
+++ src/app/_shared/components/information/error-message/error-message.component.ts
@@ -1,33 +1,46 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, OnDestroy } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
 import { UiActions } from '@store/actions/ui.actions';
 import { AppState } from '@store/reducers';
 import { selectErrorMessage } from '@store/selectors';
 import { MessageModule } from 'primeng/message';
-import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-error-message',
-  imports: [MessageModule, AsyncPipe],
+  imports: [MessageModule],
   templateUrl: './error-message.component.html',
   styleUrl: './error-message.component.scss',
 })
-export class ErrorMessageComponent {
-  store$ = inject(Store<AppState>);
-  error$ = this.store$.select(selectErrorMessage).pipe(
-    tap((value) => {
-      if (value) this.cleanMessageTtimer();
-    }),
-  );
-
+export class ErrorMessageComponent implements OnDestroy {
+  private store$ = inject(Store<AppState>);
+  errorMessage = toSignal(this.store$.select(selectErrorMessage));
   lifeTime = 5000;
+
+  private timerId: ReturnType<typeof setTimeout> | undefined;
+  constructor() {
+    effect(() => {
+      const message = this.errorMessage();
+      if (message) {
+        this.cleanMessageTimer();
+        this.timerId = setTimeout(() => {
+          this.cleanMessage();
+        }, this.lifeTime);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.cleanMessageTimer();
+  }
   cleanMessage() {
     this.store$.dispatch(UiActions.removeErrorMessage());
   }
-  cleanMessageTtimer() {
-    setTimeout(() => {
-      this.cleanMessage();
-    }, this.lifeTime);
+
+  cleanMessageTimer() {
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+      this.timerId = undefined;
+    }
   }
 }
